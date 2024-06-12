31d51e43dc1007aa102216f21f9ac615d3daca81
Federico Torres <federico.thomas.torres@gmail.com>
2024-06-11 21:38:52 -0300

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <ul>
+            <li>Instagram</li>
+            <li>X</li>
+            <li>Facebook</li>
+        </ul> 
+       
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul>
+            <li>Todo</li>
+            <li>Buzos</li>
+            <li>Remeras</li>
+            <li>Musculosas</li>
+        </ul>
+    
+    </div>
+  )
+}
+
+export default Navbar
